3fe0777d5e0de0eac3e111c33ef0362c14c3f0b6
apoloniaabramowicz <pola.abramowicz@onet.pl>
2025-01-18 19:32:50 +0100

    feat: add get matching recipes

--- backend/app/controllers/recipes_controller.ts
+++ backend/app/controllers/recipes_controller.ts
@@ -1,6 +1,7 @@
 import Recipe from '#models/recipe'
 import { updateRecipeValidator } from '#validators/recipe'
 import type { HttpContext } from '@adonisjs/core/http'
+import Amator from '#models/amator'
 
 export default class RecipesController {
   async index(_ctx: HttpContext) {
@@ -37,6 +38,44 @@ export default class RecipesController {
       return { message: 'User not authenticated.' }
     }
     const recipes = await Recipe.query().where('userId', userId)
+    return recipes
+  }
+
+  async getRecipes({ auth }: HttpContext) {
+    const userId = auth.user?.id
+    if (userId === undefined) {
+      return { message: 'User not authenticated.' }
+    }
+
+    const amator = await Amator.query()
+      .where('userId', userId)
+      .preload('nutritionalProfiles', (query) => {
+        query.preload('category', (subQuery) => {
+          subQuery.where('type', 'type_of_diet')
+        })
+      })
+      .first()
+
+    if (!amator) {
+      return Recipe.query()
+    }
+
+    const profileCategoryIds = amator.nutritionalProfiles.map((profile) => profile.categoryId)
+
+    if (profileCategoryIds.length === 0) {
+      return Recipe.query()
+    }
+
+    const recipes = await Recipe.query()
+      .whereHas('tags', (query) => {
+        query.whereIn('id', profileCategoryIds)
+      })
+      .groupBy('recipes.id')
+      .havingRaw(
+        `(SELECT COUNT(*) FROM categories INNER JOIN tags ON categories.id = tags.fk_category_id WHERE recipes.id = tags.fk_recipe_id AND categories.id IN (${profileCategoryIds.join(',')})) = ?`,
+        [profileCategoryIds.length]
+      )
+
     return recipes
   }
 }
