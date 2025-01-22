4512a9f02cd04679ab17fa9eb914094a9ae6910f
apoloniaabramowicz <pola.abramowicz@onet.pl>
2025-01-22 19:49:14 +0100

    fix: fix tile size

--- frontend/src/components/recipe-tile.tsx
+++ frontend/src/components/recipe-tile.tsx
@@ -5,20 +5,29 @@ import { Rating } from "react-simple-star-rating";
 import Link from "next/link";
 import { FavouriteToggle } from "@/components/favourite-toggle";
 import { useUser } from "@/context/user-context";
+import { TagList } from "@/components/tag-list";
 
 export function RecipeTile({ recipe }: { recipe: Recipe }) {
   const { user } = useUser();
   return (
-    <Link href={`/recipes/${recipe.id}`} className="transition hover:scale-110 max-w-56">
+    <Link href={`/recipes/${recipe.id}`} className="transition hover:scale-110 max-w-80">
       <div className="rounded-3xl p-4 shadow-lg">
         <Image src={recipe.imageUrl || "/placeholder.svg"} alt={"Image of the recipe"} width={400} height={400} />
         <div className="flex items-center justify-between w-full py-1">
           <p className="text-gray-500 font-bold">by {recipe.userId}</p>
-          <Rating className="inline-flex" size={20} readonly={true} initialValue={recipe.averageRating} />
+          <Rating className="inline-flex" size={25} readonly={true} initialValue={recipe.averageRating} />
         </div>
         <p className="text-xl font-bold">{recipe.name}</p>
+        <div className="my-4 w-full">
+          <TagList tags={[{ id: "1", type: "type", name: "vegetarian" }, {
+            id: "3",
+            type: "type",
+            name: "vegetarian"
+          }, { id: "4", type: "type", name: "vegetarian" }, { id: "8", type: "type", name: "vegetarian" }]}
+                   rows={1} />
+        </div>
         <div className="flex items-center justify-between w-full">
-          <p className="text-gray-400 text-sm font-semibold">see more {">"}</p>
+          <p className="text-gray-400 font-semibold">see more {">"}</p>
           {user?.role === "amator" && <FavouriteToggle recipe={recipe} />}
         </div>
       </div>
